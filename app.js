de266b2483fc93bdd76cc9497ac7bf4b73b2c9b2
Ayushman-500 <56505575+Ayushman-500@users.noreply.github.com>
2020-12-13 14:55:11 +0530

    Add Environment Variables

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //jshint esversion:6
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -7,6 +9,8 @@ const encrypt = require('mongoose-encryption');
 
 
 const app = express();
+
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,8 +23,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 
